24f153ebdca9cad6a2d310f070d4ec63e7e60171
Roqum <56917969+Roqum@users.noreply.github.com>
2023-12-01 10:33:12 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from "react-dom/client";
 import App from './App.jsx';
+import ScrollToTop from './components/ScrollToTop.js';
+
 
 import './scss/style.scss';
 import * as bootstrap from 'bootstrap';
@@ -12,6 +14,7 @@ const root = createRoot(appElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </React.StrictMode>
