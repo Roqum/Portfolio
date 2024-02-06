949eabdd04f9c699598e04ec7cdfe13b79c41eee
Burgi <david.burgstaller@yahoo.de>
2024-02-06 16:08:22 +0100

    edit map generator page

--- frontend/src/pages/MapGenerator.jsx
+++ frontend/src/pages/MapGenerator.jsx
@@ -9,12 +9,12 @@ const MapGenerator = (props) => {
                 <b>The map is completly created by code only</b>
                 <p>
                     This 
-                    <a className='link-underline-opacity-0 link-light link-underline-opacity-75-hover' href="https://github.com/Roqum/Precedural_HextileMap_Generator/blob/master/Assets/Scripts/Map/Hexagon.cs"> Hexagon Class </a> 
+                    <a className='link-underline-opacity-50 link-light link-underline-opacity-75-hover fc-orange' href="https://github.com/Roqum/Precedural_HextileMap_Generator/blob/master/Assets/Scripts/Map/Hexagon.cs"> Hexagon Class </a> 
                     is drawing vertices and faces, combining them into a single hex-tile mesh. The size of a single tile can be configured. I can even be made hollow so that the mesh shows a hexagon border.
                 </p>
                 <p>
                     The 
-                    <a className='link-underline-opacity-0 link-light link-underline-opacity-75-hover' href="https://github.com/Roqum/Precedural_HextileMap_Generator/blob/master/Assets/Scripts/Map/GridGenerator.cs"> map generator class </a>
+                    <a className='link-underline-opacity-50 link-light link-underline-opacity-75-hover' href="https://github.com/Roqum/Precedural_HextileMap_Generator/blob/master/Assets/Scripts/Map/GridGenerator.cs"> map generator class </a>
                     is drawing a grid and uses the Hexagon "draw" function to draw a hex-tile for each cell of the grid. It is positioning the hex-tile accordingly so that a complete map of hex-tiles is created.
                     Two "Perlin Noise" functions are used to randomly assign a land type and the corresponding  material to each of these cells. One assigns the basic terrain type and the other is used to randomly place
                     multiple groups of forest tiles onto the dry land.
@@ -22,24 +22,29 @@ const MapGenerator = (props) => {
                 <p>
                     The result is a configurable procedural generated hex-tile map which can be seen in this video:
                 </p>
-                <video autoPlay className='w-100' muted='true' loop>
+                <video autoPlay className='w-75' muted='true' loop>
                     <source src="https://david-burgstaller.com/api/video/MapGeneratorSize.mp4" type="video/mp4" />
                 </video>
                 <p>
-                    As you can see, a basic "W,A,S,D" movement is added, so the map can be explored.
+                    As you can see, a basic "W,A,S,D" movement is also added, so the map can be explored.
                 </p>
-                <p>
+                <p className='mt-5'>
                     The scale of the perlin noise can also be adjusted to variate the generated landscape as shown in this video: 
                 </p>
-                <video autoPlay className='w-100' muted='true' loop>
+                <video autoPlay className='w-75' muted='true' loop>
                     <source src="https://david-burgstaller.com/api/video/MagGeneratorScale.mp4" type="video/mp4" />
                 </video>
-                <p>
+                <p className='mt-5'>
                     Last but not least, a simple UI allows us to build two different kinds of buildings. These buildings automatically snap to the grid and adjust their position:
                 </p>
-                <video autoPlay className='w-100' muted='true' loop>
+                <video autoPlay className='w-75' muted='true' loop>
                     <source src="https://david-burgstaller.com/api/video/MapGeneratorBuilding.mp4" type="video/mp4" />
                 </video>
+                <p className='mt-5'>
+                    My goal was achieved, I got more used to the Unity engine, learned how procedural generation can be done and even learned some stuff about mesh creation and hexagon math. 
+                    <br />
+                    This procedural generated map could be a good foundation for a strategy game
+                </p>
             </div> 
         </>
     );
